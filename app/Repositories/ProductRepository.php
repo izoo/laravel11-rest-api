45f443369f4be37fab0cd9067a3633b817e36941
Izoo <aisacsmooth@gmail.com>
2024-05-23 10:32:48 +0300

    created and added ProductRepository Logic

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+
+class ProductRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function getById($id)
+    {
+        return Product::findOrFail($id);
+    }
+    public function store(array $data)
+    {
+        $product = Product::create($data);
+        return $product;
+    }
+    public function update(array $data, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($data);
+        return $product;
+    }
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+}
